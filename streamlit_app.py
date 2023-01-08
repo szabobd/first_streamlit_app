7ac8d379df8492cae1cde9a1e7deeb66712ac070
szabobd <69543350+szabobd@users.noreply.github.com>
2023-01-08 17:22:29 +0100

    adding second user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add-my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
