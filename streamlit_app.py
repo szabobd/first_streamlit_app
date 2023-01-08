da45220bf11e5c1a2c2254a5cb7887fc40d646a8
szabobd <69543350+szabobd@users.noreply.github.com>
2023-01-08 21:41:27 +0100

    changed commenting, ''' doesnt go with streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-'''add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from_streamlit')")'''
+#my_cur.execute("insert into fruit_load_list values ('from_streamlit')")'''
