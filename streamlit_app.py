bfe08c9a83e113a36d95e4cdabb6132ed0792b17
szabobd <69543350+szabobd@users.noreply.github.com>
2023-01-08 17:23:08 +0100

    fixing second user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add-my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
